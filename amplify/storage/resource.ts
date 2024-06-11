73b038005ed9116c4ea864d2df7cd51387e93f4a
Harshita Daddala <harshita.d11@gmail.com>
2024-06-11 10:17:09 -0400

    change access rule

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,12 +3,9 @@ import { defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: 'gen2-multi-cursor-demo-app',
   access: allow => ({
-    // 'room/*': [
-    //   allow.authenticated.to(['get', 'write', 'delete']),
-    // ]
-    'room/{entity_id}/*': [
-      // {entity_id} is the token that is replaced with the user identity id
-      allow.entity('identity').to(['read', 'write', 'delete'])
+    'room/*': [
+      allow.authenticated.to(['read','write','delete'])
+      // allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
 });
